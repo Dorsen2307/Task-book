48eb1ff2931ca22bb7218dacf31ec3d0b232f03e
GameFather <musorschik2008@yandex.ru>
2024-10-17 20:09:01 +0500

    Добавлена функция проверки ввода данных по типу:

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import os.path
 from menu_header import *
 
 
-
-
-
 # main
 if __name__ == '__main__':
     if not os.path.isfile('task.db'):
@@ -16,9 +13,9 @@ if __name__ == '__main__':
     while ex:
         output_menu()
 
-        input_user = input('Enter the menu item: ')
+        input_number_menu = input_with_type_check('Enter the menu item: ', int)
 
         try:
-            ex = validate_input_user(input_user)
-        except InvalidInputError:
-            print('\nInvalid Input')
+            ex = check_input_user(input_number_menu)
+        except InputValueError:
+            print('\nThere is no such menu item')
--- menu_header.py
+++ menu_header.py
@@ -12,7 +12,7 @@ def output_menu():
     0. Exit
     ------------------------------''')
 
-def output_records_tasks():
+def get_and_output_records_tasks():
     tasks, columns = view_tasks()
 
     print('\n<<<<<<<<<< List of records >>>>>>>>>>')
@@ -28,50 +28,81 @@ def output_records_tasks():
 def input_user_for_added_task():
     print('\n<<<<<<<<<< Added record >>>>>>>>>>')
 
-    title = input('Title: ')
-    description = input('Description: ')
-    due_date = input('Due date: ')
+    title = input_with_type_check('Title: ', str)
+    description = input_with_type_check('Description: ', str)
+    due_date = input_with_type_check('Due date (dd.mm.yyyy): ', 'date', '%d.%m.%Y')
 
     added_task(title, description, due_date)
 
 def deleting_entry():
     print('\n<<<<<<<<<< Deleting records >>>>>>>>>>')
 
-    output_records_tasks()
+    get_and_output_records_tasks()
 
-    id_task = input('Task ID: ')
+    id_task = input_with_type_check('Task ID: ', int)
 
     deleted_task(id_task)
 
 def changed_record_tasks():
     print('\n<<<<<<<<<< Changing records >>>>>>>>>>')
 
-    output_records_tasks()
+    get_and_output_records_tasks()
 
-    id_task = input('Enter the task id to change: ')
-    title_update = input('Title: ')
-    description_update = input('Description: ')
-    due_date_update = input('Due date: ')
-    status_update = input('Status: ')
+    id_task = input_with_type_check('Enter the task id to change: ', int)
+    title_update = input_with_type_check('Title: ', str)
+    description_update = input_with_type_check('Description: ', str)
+    due_date_update = input_with_type_check('Due date (dd.mm.yyyy): ', 'date', '%d.%m.%Y')
+    status_update = input_with_type_check('Status: ', str)
 
-    updated_date_update = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    updated_date_update = datetime.now().strftime('%Y.%m.%d %H:%M:%S')
 
     changed_task(id_task, title_update, description_update, due_date_update, status_update, updated_date_update)
 
-def validate_input_user(user_input):
-    if user_input == '0':
+def check_input_user(user_input):
+    if user_input == 0:
         return False
-    if user_input == '1':
-        output_records_tasks()
+    if user_input == 1:
+        get_and_output_records_tasks()
         return True
-    if user_input == '2':
+    if user_input == 2:
         input_user_for_added_task()
         return True
-    if user_input == '3':
+    if user_input == 3:
         deleting_entry()
         return True
-    if user_input == '4':
+    if user_input == 4:
         changed_record_tasks()
         return True
 
-    raise InvalidInputError
+    raise InputValueError
+
+def input_with_type_check(prompt, expected_type, date_format = None):
+    """
+    Запрашивает у пользователя ввод и проверяет его тип.
+
+    :param prompt: Строка, которая будет показана пользователю.
+    :param expected_type: Ожидаемый тип данных (int, float, str).
+    :return: Введенное значение, преобразованное в ожидаемый тип.
+    """
+    while True:
+        user_input = input(prompt)
+
+        try:
+            if expected_type == int:
+                return int(user_input)
+            elif expected_type == float:
+                return float(user_input)
+            elif expected_type == str:
+                return user_input
+            elif expected_type == 'date':
+                if date_format is None:
+                    print('The date format is not specified.')
+                    return
+                return datetime.strptime(user_input, date_format).date()
+            else:
+                raise ValueError("Unsupported data type.")
+        except ValueError:
+            if expected_type == 'date':
+                print(f'Error: enter the date in the format {date_format}.')
+            else:
+                print(f'Error: enter the type value {expected_type.__name__}.')
